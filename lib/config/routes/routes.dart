35461d92f92344d60c0771eb2a03480cede6351b
arbre <nayeem@arbreesolutions.com>
2024-11-28 16:55:10 +0600

    initial wih route

---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,26 @@
+
+import 'package:bloc_clean_coding/config/routes/routes_name.dart';
+import 'package:flutter/material.dart';
+
+import '../../views/view.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.splashScreen:
+        return MaterialPageRoute(builder: (context) => SplashScreen());
+      case RoutesName.homeScreen:
+        return MaterialPageRoute(builder: (context) => HomeScreen());
+      case RoutesName.loginScreen:
+        return MaterialPageRoute(builder: (context) => LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (context) {
+          return Scaffold(
+            body: Center(
+              child: Text("No Route generated"),
+            ),
+          );
+        });
+    }
+  }
+}
